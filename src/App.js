2d79eb31e6697b930eadf5496e5a562a561774ca
abdikasa <abdikasa@sheridancollege.ca>
2020-09-05 13:29:37 -0400

    Completed: using single responsibility principle to seperate the server related code.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const App = () => {
   const [notes, setNotes] = useState([]);
@@ -8,9 +8,7 @@ const App = () => {
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    axios.get("http://localhost:3001/notes").then((response) => {
-      setNotes(response.data);
-    });
+    noteService.getAll().then((response) => setNotes(response.data));
   }, []);
 
   const addNote = (event) => {
@@ -22,21 +20,21 @@ const App = () => {
       id: notes.length + 1,
     };
 
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+    noteService.create(noteObject).then((response) => {
       setNotes(notes.concat(response.data));
       setNewNote("");
     });
   };
 
   const toggleChange = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     //find the object, return object by using id.
     const note = notes.find((n) => n.id === id);
     const changeNote = { ...note, important: !note.important };
 
     //put or patch can be used.
-    axios
-      .put(url, changeNote)
+
+    noteService
+      .update(id, changeNote)
       .then((response) =>
         setNotes(notes.map((note) => (note.id !== id ? note : response.data)))
       );
